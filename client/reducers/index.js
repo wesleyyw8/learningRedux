fd60640167c7bad0db3d21869a7113c3f30e8bc5
wesley <wesleyyw8@gmail.com>
2017-07-25 22:32:44 +0100

    .

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
-import { routerReducer } from 'react-reducer-redux';
+import { routerReducer } from 'react-router-redux';
 
 import posts from './posts';
 import comments from './comments';
 
 const rootReducer = combineReducers({
-  posts, comments, router: routerReducer
+  posts, comments, routing: routerReducer
 })
 
-export default rootReducer;
+export default rootReducer;    
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,18 +5,22 @@ import css from './styles/style.styl';
 
 //import components
 import Main from './components/Main';
-import Single from './components/Single';
+import Single from './components/Single'; 
 import PhotoGrid from './components/Photogrid';
 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'; 
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view/:postId" component={Single }></Route>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single }></Route>
+      </Route>
+    </Router>
+  </Provider>
 )
 
 
